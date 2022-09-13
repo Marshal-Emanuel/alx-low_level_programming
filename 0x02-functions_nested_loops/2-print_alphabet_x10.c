5ae19aac9111843521a628bbcb932134ee4934c1
Marshal-Emanuel <marshcodes@gmail.com>
2022-09-14 02:51:01 +0300

    4 rstructure

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print alphabet
  *
@@ -9,8 +8,6 @@ void print_alphabet_x10(void)
 {
 int alphabet;
 int count;
-
-
 count = 0;
 while (count < 10)
 {
@@ -18,10 +15,7 @@ for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-
 count++;
 _putchar('\n');
 }
-
-
 }
