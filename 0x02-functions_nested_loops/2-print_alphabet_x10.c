600101478daf9c71a0342dee6e870998d055bd01
Marshal-Emanuel <marshcodes@gmail.com>
2022-09-14 03:02:17 +0300

    no 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print alphabet
- *
- * Return: Always 0.
- */
+* print_alphabet_x10 - print alphabet
+*
+* Return: Always 0.
+*/
 void print_alphabet_x10(void)
 {
 int alphabet;
@@ -17,5 +17,4 @@ _putchar(alphabet);
 }
 count++;
 _putchar('\n');
-}
 }
