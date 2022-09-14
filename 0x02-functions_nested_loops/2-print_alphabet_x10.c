b11c60b08daae8bf2cf687ea7c6359b80c2ab2ee
Marshal-Emanuel <marshcodes@gmail.com>
2022-09-14 08:31:24 +0300

    white space removed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
- * print_alphabet_x10 - print alphabet
- *
- * Return: Always 0.
- */
+* @brief 
+* 
+*/
+*
+* Return: Always 0.
+*/
 void print_alphabet_x10(void)
 {
 int alphabet;
 int count;
-
-
 count = 0;
 while (count < 10)
 {
@@ -18,10 +17,7 @@ for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
-
 count++;
 _putchar('\n');
 }
-
-
 }
