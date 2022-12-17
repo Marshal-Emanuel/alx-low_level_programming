bf16290aa6c40c16d7eaa48ddcaddf36d32cd7bd
Emanuel Marshal <marshcodes@gmail.com>
2022-12-17 14:08:31 +0300

    final touch

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,7 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef main_h
+#define main_h
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -12,7 +11,7 @@ int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
-void _puts(char *str);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
@@ -26,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* main_h */
